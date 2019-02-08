2d14ad8a0ddb094904c39247d17be4b008f8ceb9
Pantufla <antoniojss94@gmail.com>
2019-02-07 18:45:36 -0600

    adding CurrentConditions class

---
+++ AppCurrentConditions.java
@@ -0,0 +1,25 @@
+public class AppCurrentConditions implements Observer, DisplayElements {
+    
+    private float temperature;
+    private float humidity;
+    private float pressure;
+    private Subject weatherData;
+
+    public AppCurrentConditions(Subject weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver (this);
+    }
+
+    public void update(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+        display();
+    }
+
+    public void display() {
+        System.out.println("Current conditions: " + temperature + 
+                            "F degrees, " + humidity + "% humidity, and " + 
+                            pressure + "pressure");
+    }
+}
