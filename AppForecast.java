fd67c4b27d6da16d04643a54655a375e12b2617d
LuisFLuna <luigifleed2@hotmail.com>
2019-02-07 18:22:31 -0600

    Added Class AppForecast

---
+++ AppForecast.java
@@ -0,0 +1,13 @@
+//Luis Fernando Flores Luna   A01324611
+
+public class AppForecast implements Observer{
+  private WeatherData wd;
+
+  /*
+  Method update
+  */
+
+  public void update(){
+    System.out.println("Updated");
+  }
+}
