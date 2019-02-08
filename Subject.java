090059720b0df92f191ead70f477a9dae387ba7a
Benny Ruiz Jimenez <a01328177@itesm.mx>
2019-02-07 18:36:50 -0600

    Update Subject.java

--- Subject.java
+++ Subject.java
@@ -2,6 +2,6 @@ interface Subject
 {
     public void registerObserver(Observer o);
     public void removeObserver(Observer o);
-    public void notifyObserver(Observer o);
+    public void notifyObserver();
     
 }
